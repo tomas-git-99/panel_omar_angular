38edb6de9a33e7304a0d8f090ad05549f07e4b4f
Tomas Lopez <tomasgit@MacBook-Air-de-Tomas.local>
2022-05-05 01:05:57 -0300

    casi final2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,7 @@ export const environment = {
 
   //urlBackendProduccion: 'http://192.168.1.49:3000/'
   //urlBackendProduccion: 'http://137.184.178.187:3000/'
+  
   urlBackendProduccion: 'http://localhost:3000/'
 
 };
